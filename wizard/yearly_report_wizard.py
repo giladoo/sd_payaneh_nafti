a2b6465a9dd38a219abb62735b5eedc04173e3d5
arash <arash@Arash.arash>
2024-04-19 13:34:58 -0400

    spgr

---
+++ wizard/yearly_report_wizard.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo import Command
+from colorama import Fore
+from datetime import datetime, date
+from datetime import timedelta
+from odoo.exceptions import ValidationError, UserError
+import jdatetime
+from . import get_calendare as gc
+import pytz
+# #############################################################################
+class SdPayanehNaftiReportMonthly(models.TransientModel):
+    _name = 'sd_payaneh_nafti.report.monthly_report'
+    _description = 'Monthly Report'
+
+    month_start = fields.Selection(lambda self: gc.get_months_pr() if self.env.context.get('lang') == 'fa_IR' else gc.get_months(),
+                             string='Month', required=True,
+                             default=lambda self: self._month_selector())
+    year_start = fields.Selection(lambda self: gc.get_years_pr() if self.env.context.get('lang') == 'fa_IR' else gc.get_years(),
+                            string='Year', required=True,
+                            default=lambda self: self._year_selector())
+
+    month = fields.Selection(lambda self: gc.get_months_pr() if self.env.context.get('lang') == 'fa_IR' else gc.get_months(),
+                             string='Month', required=True,
+                             default=lambda self: self._month_selector())
+    year = fields.Selection(lambda self: gc.get_years_pr() if self.env.context.get('lang') == 'fa_IR' else gc.get_years(),
+                            string='Year', required=True,
+                            default=lambda self: self._year_selector())
+
+    start_date = fields.Date(required=True, default=lambda self: datetime.now(pytz.timezone(self.env.context.get('tz'))) )
+    calendar = fields.Selection([('fa_IR', 'Persian'), ('en_US', 'Gregorian')],
+                                default=lambda self: 'fa_IR' if self.env.context.get('lang') == 'fa_IR' else 'en_US')
+
+    # #############################################################################
+    def monthly_report(self):
+        read_form = self.read()[0]
+        data = {'form_data': read_form}
+
+        return self.env.ref('sd_payaneh_nafti.monthly_report').report_action(self, data=data)
+    # #############################################################################
+    def monthly_xls_report(self):
+        read_form = self.read()[0]
+        data = {'form_data': read_form}
+
+        return self.env.ref('sd_payaneh_nafti.monthly_xls_report').report_action(self, data=data)
+
+
+    def _year_selector(self):
+        # todo: timezone is needed to make sure date after 8 pm is correct
+        this_date = datetime.now(pytz.timezone(self.env.context.get('tz')))
+        if self.env.context.get('lang') == 'fa_IR':
+            s_this_year = jdatetime.date.fromgregorian(date=this_date).strftime("%Y")
+        else:
+            s_this_year = this_date.strftime("%Y")
+        return s_this_year
+    def _month_selector(self):
+        # todo: timezone is needed to make sure date after 8 pm is correct
+        this_date = datetime.now(pytz.timezone(self.env.context.get('tz')))
+        if self.env.context.get('lang') == 'fa_IR':
+            s_this_month = jdatetime.date.fromgregorian(date=this_date).strftime("%m")
+        else:
+            s_this_month = this_date.strftime("%m")
+        return s_this_month
