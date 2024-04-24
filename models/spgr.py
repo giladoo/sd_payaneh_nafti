54f742aed145feb0797260f2aa817252f0baa3e3
arash <arash@Arash.arash>
2024-04-24 15:20:16 -0400

    spgr default value

--- models/spgr.py
+++ models/spgr.py
@@ -11,8 +11,11 @@ class SdPayanehNaftiSpgr(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name = 'spgr'
 
+    def _spgr_default(self, param):
+        return self.search([], order="id desc", limit=1)[param]
+
     active = fields.Boolean(default=True)
-    spgr = fields.Float(required=True, digits=[1, 4])
+    spgr = fields.Float(required=True, digits=[1, 4], default=lambda self: self._spgr_default('spgr'))
     spgr_date = fields.Date(required=True, default=lambda self: datetime.now(pytz.timezone(self.env.context.get('tz', 'Asia/Tehran'))) )
     api_a = fields.Float(digits=[1, 2], store=True, required=True,)
     centralized_container = fields.Selection([('a', 'A'),
@@ -23,13 +26,13 @@ class SdPayanehNaftiSpgr(models.Model):
                                               ('f', 'F'),
                                               ('g', 'G'),
                                               ('h', 'H'),
-                                              ], required=True, default='h', tracking=True)
-    vapour_pressure = fields.Float(required=True)
-    salt_content = fields.Float(required=True)
-    mercaptans = fields.Integer(required=True)
-    h2s = fields.Selection([('trace', 'TRACE')], default='trace', required=True)
-    sulphur = fields.Float(required=True, digits=[1, 3])
-    water_content = fields.Selection([('nil', 'NIL')], default='nil', required=True)
+                                              ], required=True, tracking=True, default=lambda self: self._spgr_default('centralized_container'))
+    vapour_pressure = fields.Float(required=True, default=lambda self: self._spgr_default('vapour_pressure'))
+    salt_content = fields.Float(required=True, default=lambda self: self._spgr_default('salt_content'))
+    mercaptans = fields.Integer(required=True, default=lambda self: self._spgr_default('mercaptans'))
+    h2s = fields.Selection([('trace', 'TRACE')], required=True, default=lambda self: self._spgr_default('h2s'))
+    sulphur = fields.Float(required=True, digits=[1, 3], default=lambda self: self._spgr_default('sulphur'))
+    water_content = fields.Selection([('nil', 'NIL')], required=True, default=lambda self: self._spgr_default('water_content'))
 
     description = fields.Char()
 
